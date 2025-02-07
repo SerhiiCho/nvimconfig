911328fd1106a4a4639c81182f279aec5d35b92f
SerhiiCho <serhiicho@protonmail.com>
2025-02-07 22:04:15 +0200

    Add go autoimport functionality

---
+++ lua/config/go_import.lua
@@ -0,0 +1,17 @@
+local importGoPackages = function()
+	-- Save cursor position
+	local save_cursor = vim.api.nvim_win_get_cursor(0)
+
+	-- Run goimports
+	vim.cmd("silent! %!goimports")
+
+	-- Move one line down after restoring position
+	-- Ensure within bounds
+	local new_line = math.min(save_cursor[1] + 1, vim.api.nvim_buf_line_count(0))
+	vim.api.nvim_win_set_cursor(0, { new_line, save_cursor[2] })
+end
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*.go",
+    callback = importGoPackages,
+})
