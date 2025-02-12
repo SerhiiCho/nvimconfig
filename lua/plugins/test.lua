42b0570ade77c0acd4772da8cba9f53b97e5950f
SerhiiCho <serhiicho@protonmail.com>
2025-02-12 15:23:52 +0200

    Remove nvim-go and install nvim-test plugin

---
+++ lua/plugins/test.lua
@@ -0,0 +1,26 @@
+-- https://github.com/klen/nvim-test
+
+return {
+	"klen/nvim-test",
+	config = function()
+		vim.keymap.set("n", "<leader>tn", '<cmd>TestNearest<CR>', {
+			desc = "[T]est [N]earest",
+		})
+
+		vim.keymap.set("n", "<leader>tl", '<cmd>TestLast<CR>', {
+			desc = "[T]est [L]ast",
+		})
+
+		vim.keymap.set("n", "<leader>tf", '<cmd>TestFile<CR>', {
+			desc = "[T]est [F]ile",
+		})
+
+		vim.keymap.set("n", "<leader>ts", "<cmd>TestSuite<CR>", {
+			desc = "[T]est [S]uite",
+		})
+
+		vim.keymap.set("n", "<leader>te", "<cmd>TestEdit<CR>", {
+			desc = "[T]est [E]dit for the current file",
+		})
+	end,
+}
