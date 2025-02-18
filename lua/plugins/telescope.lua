bde62d1c7362c5307385d6082060df097b5b91c1
SerhiiCho <serhiicho@protonmail.com>
2025-02-18 14:58:48 +0200

    Improve configurations

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,11 +3,15 @@
 return {
 	"nvim-telescope/telescope.nvim",
 	tag = "0.1.8",
-	dependencies = { "nvim-lua/plenary.nvim" },
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+	},
 	config = function()
 		local builtin = require("telescope.builtin")
 		local telescope = require("telescope")
 		local actions = require("telescope.actions")
+		local themes = require("telescope.themes")
 
 		telescope.setup({
 			defaults = {
@@ -20,44 +24,39 @@ return {
 			},
 		})
 
-		vim.keymap.set("n", "<C-p>", builtin.git_files, {
-			noremap = true,
-			desc = "Search files not ignored by git",
-		})
+		local get_opts = function(desc)
+			return {
+				noremap = true,
+				desc = desc,
+			}
+		end
 
-		vim.keymap.set("n", "<leader>sf", builtin.find_files, {
-			noremap = true,
-			desc = "[S]earch [F]iles",
-		})
+		vim.keymap.set("n", "<C-p>", function()
+			builtin.git_files(themes.get_ivy())
+		end, get_opts("Search Git Files"))
 
-		vim.keymap.set("n", "<leader>sg", builtin.live_grep, {
-			noremap = true,
-			desc = "[S]earch by [G]rep",
-		})
+		vim.keymap.set("n", "<leader>sf", function()
+			builtin.find_files(themes.get_ivy())
+		end, get_opts("[S]earch [F]iles"))
 
-		vim.keymap.set("n", "<leader>sb", builtin.buffers, {
-			noremap = true,
-			desc = "[S]earch [B]uffers",
-		})
+		vim.keymap.set("n", "<leader>sg", function()
+			builtin.live_grep(themes.get_ivy())
+		end, get_opts("[S]earch by [G]rep"))
 
-		vim.keymap.set("n", "<leader>so", "<cmd>Telescope lsp_document_symbols<CR>", {
-			noremap = true,
-			desc = "[S]earch [O]utline",
-		})
+		vim.keymap.set("n", "<leader>sb", function()
+			builtin.buffers(themes.get_ivy())
+		end, get_opts("[S]earch [B]uffers"))
 
-		vim.keymap.set("n", "<leader>s.", builtin.oldfiles, {
-			noremap = true,
-			desc = '[S]earch Recent Files ("." for repeat)',
-		})
+		vim.keymap.set("n", "<leader>so", function()
+			builtin.lsp_document_symbols(themes.get_ivy())
+		end, get_opts("[S]earch [O]utline"))
 
-		vim.keymap.set("n", "<leader>sh", builtin.help_tags, {
-			noremap = true,
-			desc = "[S]earch [H]elp",
-		})
+		vim.keymap.set("n", "<leader>s.", function()
+			builtin.oldfiles(themes.get_ivy())
+		end, get_opts('[S]earch Recent Files ("." for repeat)'))
 
-		-- vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
-		-- vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
-		-- vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
-		-- vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
+		vim.keymap.set("n", "<leader>sh", function()
+			builtin.help_tags(themes.get_ivy())
+		end, get_opts("[S]earch [H]elp"))
 	end,
 }
