86cbc741d6b696a118de86e995ae2d0365d76642
SerhiiCho <serhiicho@protonmail.com>
2025-02-20 16:53:18 +0200

    Add more remaps

--- lua/serhii/remap.lua
+++ lua/serhii/remap.lua
@@ -8,6 +8,21 @@ vim.keymap.set("n", "<leader>ex", vim.cmd.Ex, {
     noremap = true,
 })
 
+vim.keymap.set("n", "<leader>pr", "<cmd>silent !tmux neww tmux-sessionizer<CR>", {
+    desc = "Open a [pr]oject sessionizer",
+})
+
+vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>", {
+    desc = "Search and replace the current word under the cursor",
+    noremap = true,
+})
+
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", {
+    desc = "Make the current file executable",
+    silent = true,
+    noremap = true,
+})
+
 -- Excecute files for different languages
 vim.keymap.set("n", ",,", ":!php %<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", ",py", ":!python3 %<CR>", { noremap = true, silent = true })
