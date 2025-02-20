54704aed9ee0d2136bd4fffcca41c3ac37173e7d
SerhiiCho <serhiicho@protonmail.com>
2025-02-20 19:30:57 +0200

    Changes

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -20,7 +20,6 @@ vim.opt.rtp:prepend(lazypath)
 -- Setup lazy.nvim
 require("lazy").setup({
 	spec = { { import = "plugins" } },
-	install = { colorscheme = { "material" } },
 	checker = {
 		enabled = true,
 		notify = false,
