6f495228251b113fc64acf8494f5be4d2e55044a
SerhiiCho <serhiicho@protonmail.com>
2025-02-25 14:25:58 +0200

    Format file

--- lua/plugins/nonels.lua
+++ lua/plugins/nonels.lua
@@ -1,26 +1,26 @@
 -- https://github.com/nvimtools/none-ls.nvim
 
 return {
-    "nvimtools/none-ls.nvim",
-    config = function()
-        local null_ls = require("null-ls")
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
 
-        null_ls.setup({
-            sources = {
-                null_ls.builtins.formatting.stylua,
-                null_ls.builtins.diagnostics.phpstan,
-                null_ls.builtins.formatting.pint,
-                null_ls.builtins.formatting.prettier.with({
-                    runtime_condition = function(params)
-                        local utils = require("null-ls.utils")
-                        local root =
-                            utils.root_pattern(".prettierrc", "Makefile", ".git", "package.json")(params.bufname)
-                        return root and utils.path.exists(utils.path.join(root, ".prettierrc"))
-                    end,
-                }),
-            },
-        })
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.diagnostics.phpstan,
+				null_ls.builtins.formatting.pint,
+				null_ls.builtins.formatting.prettier.with({
+					runtime_condition = function(params)
+						local utils = require("null-ls.utils")
+						local root =
+							utils.root_pattern(".prettierrc", "Makefile", ".git", "package.json")(params.bufname)
+						return root and utils.path.exists(utils.path.join(root, ".prettierrc"))
+					end,
+				}),
+			},
+		})
 
-        vim.keymap.set("n", "<leader>ff", vim.lsp.buf.format, {})
-    end,
+		vim.keymap.set("n", "<leader>ff", vim.lsp.buf.format, {})
+	end,
 }
