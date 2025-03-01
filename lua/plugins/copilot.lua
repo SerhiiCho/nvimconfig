53032773e1c6d9076b6c85d3d2a8440b5b26745a
SerhiiCho <serhiicho@protonmail.com>
2025-03-01 14:10:01 +0200

    Add copilot chat

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,9 +1,32 @@
 -- https://github.com/github/copilot.vim
+-- https://github.com/CopilotC-Nvim/CopilotChat.nvim
 
 return {
-	"github/copilot.vim",
-	config = function()
-		vim.keymap.set("n", "<leader>cd", ":Copilot disable<CR>", {})
-		vim.keymap.set("n", "<leader>ce", ":Copilot enable<CR>", {})
-	end,
+	{
+		"github/copilot.vim",
+		config = function()
+			vim.keymap.set("n", "<leader>cd", ":Copilot disable<CR>", {
+				desc = "Disable Copilot",
+			})
+
+			vim.keymap.set("n", "<leader>ce", ":Copilot enable<CR>", {
+				desc = "Enable Copilot",
+			})
+		end,
+	},
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		dependencies = {
+			{ "github/copilot.vim" },
+			{ "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+		},
+		build = "make tiktoken", -- Only on MacOS or Linux
+		config = function()
+			require("CopilotChat").setup({})
+
+			vim.keymap.set("n", "<leader>co", "<CMD>CopilotChatToggle<CR>", {
+				desc = "Toggle Copilot Chat",
+			})
+		end,
+	},
 }
