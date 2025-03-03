3788ee8ca25b922912540c6aed5878dda5c17a76
SerhiiCho <serhiicho@protonmail.com>
2025-03-03 12:59:20 +0200

    Format files

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -1,11 +1,11 @@
 -- Highlight when yanking text
 -- Try it with `yap` in normal mode
 vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking text",
-	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
+    desc = "Highlight when yanking text",
+    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
 })
 
 -- Format file before writing
@@ -20,7 +20,7 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 vim.api.nvim_create_autocmd("FileType", {
     pattern = { "markdown", "text" },
     callback = function()
-        vim.opt.wrap = true    -- Enable word wrap
+        vim.opt.wrap = true -- Enable word wrap
         vim.opt.linebreak = true -- Prevent breaking words in half
     end,
 })
--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,36 +1,14 @@
 -- https://github.com/github/copilot.vim
--- https://github.com/CopilotC-Nvim/CopilotChat.nvim
 
 return {
-	{
-		"github/copilot.vim",
-		config = function()
-			vim.keymap.set("n", "<leader>cd", "<CMD>Copilot disable<CR>", {
-				desc = "Disable Copilot",
-			})
+	"github/copilot.vim",
+	config = function()
+		vim.keymap.set("n", "<leader>cd", "<CMD>Copilot disable<CR>", {
+			desc = "Disable Copilot",
+		})
 
-			vim.keymap.set("n", "<leader>ce", "<CMD>Copilot enable<CR>", {
-				desc = "Enable Copilot",
-			})
-		end,
-	},
-	{
-		"CopilotC-Nvim/CopilotChat.nvim",
-		dependencies = {
-			{ "github/copilot.vim" },
-			{ "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
-		},
-		build = "make tiktoken", -- Only on MacOS or Linux
-		config = function()
-			require("CopilotChat").setup({})
-
-			vim.keymap.set("n", "<leader>co", "<CMD>CopilotChatToggle<CR>", {
-				desc = "[C]opilot [O]pen Chat",
-			})
-
-			vim.keymap.set("n", "<leader>cs", "<CMD>CopilotChatStop<CR>", {
-				desc = "[C]opilot [S]top writing response",
-			})
-		end,
-	},
+		vim.keymap.set("n", "<leader>ce", "<CMD>Copilot enable<CR>", {
+			desc = "Enable Copilot",
+		})
+	end,
 }
