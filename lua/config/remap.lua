d44eb9af0efe78b95f63ecb1fe6d2f53666349aa
SerhiiCho <serhiicho@protonmail.com>
2025-03-03 12:57:50 +0200

    Add yielding into system clipboard

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,11 +1,6 @@
 -- Remove highlight after search has been done
 vim.keymap.set("n", "<CR>", "<cmd>nohlsearch<CR>")
 
-vim.keymap.set("v", "<C-c>", '"+y', {
-	desc = "Copy from visual mode to system clipboard",
-	noremap = true,
-})
-
 vim.keymap.set("n", "<leader>xx", "<cmd>!chmod +x %<CR>", {
 	desc = "Make the current file executable",
 	silent = true,
