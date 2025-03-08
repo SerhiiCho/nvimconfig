537bc7ae6257b210e86ab0bddcf1859485391a26
SerhiiCho <serhiicho@protonmail.com>
2025-03-08 15:43:05 +0200

    Add :Reload command

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -24,3 +24,30 @@ vim.api.nvim_create_autocmd("FileType", {
         vim.opt.linebreak = true -- Prevent breaking words in half
     end,
 })
+
+-- Define the :Reload command
+-- Function to detect system theme and set colorscheme
+local function set_colorscheme()
+    local systemTheme = io.popen("defaults read -g AppleInterfaceStyle 2>/dev/null"):read("*a")
+
+    if systemTheme:match("Dark") then
+        vim.cmd("colorscheme github_dark_dimmed") -- Use dark theme
+    else
+        vim.cmd("colorscheme github_light") -- Use light theme
+    end
+end
+
+-- Define the :Reload command
+vim.api.nvim_create_user_command("Reload", function()
+    -- Save all buffers
+    vim.cmd("wa")
+
+    -- Source the init.lua file to reload the configuration
+    vim.cmd("source $MYVIMRC")
+
+    -- Reapply the colorscheme based on system theme
+    set_colorscheme()
+
+    -- Notify the user
+    print("Neovim configuration reloaded and theme updated!")
+end, {})
